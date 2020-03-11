aba219783d558d9210a1520f140a34c59d594919
Mauricio Sampaio <mausampaio@gmail.com>
2020-03-10 23:33:11 -0300

    Criando botões esconder e mostrar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,35 @@
 const cards = document.querySelectorAll('.card');
+const toggleIngredients = document.querySelector('.toggle-ingredients');
+const togglePreparation = document.querySelector('.toggle-preparation');
+const toggleInfo = document.querySelector('.toggle-info');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
         const recipeID = card.getAttribute("id");
         window.location.href = `/recipes/${recipeID}`;
     });
+};
+
+toggleIngredients.addEventListener("click", function() {
+    hide('ingredients', toggleIngredients);
+});
+
+togglePreparation.addEventListener('click', function() {
+    hide('preparation', togglePreparation); 
+});
+
+toggleInfo.addEventListener('click', function() {
+    hide('info', toggleInfo);
+});
+
+function hide(id, toggle) {
+    const element = document.getElementById(id);
+
+    if (element.classList.contains('hide')) {
+        element.classList.remove('hide');
+        toggle.innerHTML = "ESSCONDER";
+    } else {
+        element.classList.add('hide');
+        toggle.innerHTML = "MOSTRAR";
+    };
 };
