57dd3c61afc196128735490345998e7424cab3db
Mauricio Sampaio <mausampaio@gmail.com>
2020-06-19 10:08:28 -0300

    Separating admin routes

---
+++ src/routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = express.Router();
+const multer = require('../app/middlewares/multer');
+const recipes = require('../app/controllers/recipes');
+const chefs = require('../app/controllers/chefs');
+
+routes.get("/recipes", recipes.index);
+routes.get("/recipes/create", recipes.create);
+routes.get("/recipes/:id", recipes.show);
+routes.get("/recipes/:id/edit", recipes.edit);
+routes.post("/recipes", multer.array("photos", 5), recipes.post);
+routes.put("/recipes", multer.array("photos", 5), recipes.put);
+routes.delete("/recipes", recipes.delete);
+
+routes.get("/chefs", chefs.index);
+routes.get("/chefs/create", chefs.create);
+routes.get("/chefs/:id", chefs.show);
+routes.get("/chefs/:id/edit", chefs.edit);
+routes.post("/chefs", multer.array("avatar", 1), chefs.post);
+routes.put("/chefs", multer.array("avatar", 1), chefs.put);
+routes.delete("/chefs", chefs.delete);
+
+module.exports = routes;
