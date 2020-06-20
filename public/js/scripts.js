517a6d4eb74995c54a44ffadf83e1f49fa27e029
Mauricio Sampaio <mausampaio@gmail.com>
2020-06-20 12:45:47 -0300

    Created validation on User create form

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -321,3 +321,38 @@ const AvatarUpload = {
         reader.readAsDataURL(filelist[0]);
     }
 };
+
+const Validate = {
+    apply(input, func) {  
+        Validate.clearErrors(input);
+
+        let results = input.value = Validate[func](input.value);
+        input.value = results.value;
+        
+        if (results.error) Validate.displayError(input, results.error);
+
+    },
+    displayError(input, error) {
+        const div = document.createElement('div');
+        div.classList.add('error');
+        div.innerHTML = error;
+        input.parentNode.appendChild(div);
+        input.focus();
+    },
+    clearErrors(input) {
+        const errorDiv = input.parentNode.querySelector(".error");
+        if (errorDiv) errorDiv.remove();
+    },
+    isEmail(value) {
+        let error = null;
+
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if (!value.match(mailFormat)) error = "Email inválido";
+
+        return {
+            error,
+            value
+        };
+    }
+};
